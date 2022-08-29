fd37543292b0cd23c55f711a76fcfb9c0388b8c8
prishaanand <60487925+prishaanand@users.noreply.github.com>
2022-08-28 21:54:37 -0700

    Enable the WKWebView - load data from website to display on the app

--- Sororite Vintage App/ContentView.swift
+++ Sororite Vintage App/ContentView.swift
@@ -6,15 +6,26 @@
 //
 
 import SwiftUI
+import WebKit
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello")
+struct ContentView: UIViewRepresentable {
+    
+    var url: URL
+    
+    func makeUIView(context: Context) -> some UIView {
+            
+        let webView = WKWebView()
+            
+        // Create the request and load it
+        let request = URLRequest(url: url)
+        webView.load(request)
+            
+        return webView
     }
+        
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        return
+    }
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
--- Sororite Vintage App/Sororite_Vintage_App.swift
+++ Sororite Vintage App/Sororite_Vintage_App.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Prisha Anand on 8/28/22.
 //
+// Description: Uses the UIKit feature WKWebView in order to display the exisiting shopping website as an app-friendly version.
 
 import SwiftUI
 
@@ -11,7 +12,8 @@ import SwiftUI
 struct Sororite_Vintage_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(url: URL(string: "https://sororitevintage.com/")!)
+                .edgesIgnoringSafeArea(.bottom)
         }
     }
 }
