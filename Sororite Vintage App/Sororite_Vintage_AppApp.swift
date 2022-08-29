ce1134011971edd089505f5f34ec2747c1ff5e68
prishaanand <60487925+prishaanand@users.noreply.github.com>
2022-08-28 21:42:52 -0700

    Initial Commit

---
+++ Sororite Vintage App/Sororite_Vintage_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sororite_Vintage_AppApp.swift
+//  Sororite Vintage App
+//
+//  Created by Prisha Anand on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Sororite_Vintage_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
